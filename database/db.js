80ce9da4c172598816b0999576914ca45ca37f25
DIVYAM VERMA <113888422+divyam8175@users.noreply.github.com>
2025-03-19 01:53:24 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose'
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 export const dbConnection = () => {
