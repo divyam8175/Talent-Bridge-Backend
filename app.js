ee78dbe6520ccdaeb306ae0183040a0a33d1984f
DIVYAM VERMA <divyamverma90@gmail.com>
2025-03-19 02:12:05 +0530

    update

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ const app = express()
 dotenv.config({path:'./config/config.env'})
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: "https://your-frontend-url.onrender.com", // Replace with your actual frontend Render URL
     credentials: true
-}))
+  }));
 
 app.use(cookieParser())
 app.use(express.json())
