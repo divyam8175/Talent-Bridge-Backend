05827dcab8a2106060f892327546125fd4df067a
DIVYAM VERMA <divyamverma90@gmail.com>
2025-03-19 23:02:29 +0530

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,11 +1,12 @@
 export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken()
     const options = {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: true,
-        sameSite: "None"
-    }
+        secure: process.env.NODE_ENV === "production", // Use secure cookies only in production
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax" // or "Strict" based on your needs
+    };
+    
     res.status(statusCode).cookie('token',token,options).json({
         success: true,
         user,
